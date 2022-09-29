cface30942a5bc6de9a1d57e5ddb9d2af2941b66
Yann <doublet.yann@orange.fr>
2022-09-29 16:37:45 +0200

    Ajout carousel + corrections diverses

--- public/js/script.js
+++ public/js/script.js
@@ -1,41 +1,5 @@
 console.log("Hello World!");
 
-// window.onload=function(){
-//     var bouton = document.getElementById('btnMenu');
-//     var nav = document.getElementById('nav');
-//     bouton.onclick = function(e){
-//         if(nav.style.display=="block"){
-//             nav.style.display="none";
-//         }else{
-//             nav.style.display="block";
-//         }
-//     };
-// };
-
-// function afficheMenu(obj){
-   
-//     var idMenu     = obj.id;
-//     var idSousMenu = 'sous' + idMenu;
-//     var sousMenu   = document.getElementById(idSousMenu);
-    
-//     /*****************************************************/
-//     /**   on cache tous les sous-menus pour n'afficher    **/
-//     /** que celui dont le menu correspondant est cliqué **/
-//     /** où 4 correspond au nombre de sous-menus         **/
-//     /*****************************************************/
-  
-    
-//     if(sousMenu){
-//        //alert(sousMenu.style.display);
-//        if(sousMenu.style.display == "block"){
-//           sousMenu.style.display = "none";
-//        }
-//        else{
-//           sousMenu.style.display = "block";
-//        }
-//     }
-//  }
-
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
@@ -55,4 +19,3 @@ function myFunction() {
       }
     }
   }
-       
