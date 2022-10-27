a88840efe7bb4d43cda609750ce0a1e01fc144d5
Yann <doublet.yann@orange.fr>
2022-10-27 09:40:34 +0200

    style bouton

--- src/Form/UsersType.php
+++ src/Form/UsersType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,8 +15,9 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, ['label' => ' ', 'attr' => array('placeholder' => 'Mot de passe')])
+            ->add('name', TextType::class, ['label' => ' ', 'attr' => array('placeholder' => 'Nom')])
             ->add('email', EmailType::class, ['label' => ' ', 'attr' => array('placeholder' => 'Email')])
+            ->add('password', PasswordType::class)
         ;
     }
 
