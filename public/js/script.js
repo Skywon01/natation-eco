7f46d6cdad1bfe6dbf986fca825dfe876b898a47
Skywon <doublet.yann@orange.fr>
2025-01-17 15:32:01 +0100

    changed style details

--- public/js/script.js
+++ public/js/script.js
@@ -1,52 +1,52 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+//
+// /* Au clic sur le bouton on change la classe */
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+//   }
+//
+//   // Au clic hors de la zone, le menu disparait
+//   window.onclick= function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//       var dropdowns = document.getElementsByClassName("dropdown-content");
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+// }
 
-/* Au clic sur le bouton on change la classe */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Au clic hors de la zone, le menu disparait
-  window.onclick= function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-}
-
-'use strict'
-/* eslint-env browser */
-
-const fermerMenu = () => {
-  // Récupérer le menu
-  const input = document.getElementById('menu-cb')
-  input.checked = false
-
-  const fenetreNode = document.getElementById('menu-cote')
-  fenetreNode.remove()
-}
-
-const changerEtatMenu = () => {
-  // Récupérer le menu
-  const input = document.getElementById('menu-cb')
-  const actif = input.checked
-
-  if (actif) {
-    const fenetreNode = document.createElement('div')
-    fenetreNode.id = 'menu-cote'
-    fenetreNode.className = 'menu-cote'
-    fenetreNode.addEventListener('click', fermerMenu)
-    document.body.appendChild(fenetreNode)
-  } else {
-    const fenetreNode = document.getElementById('menu-cote')
-    fenetreNode.remove()
-  }
-}
-
-const input = document.getElementById('menu-cb')
-input.addEventListener('click', changerEtatMenu)
+// 'use strict'
+// /* eslint-env browser */
+//
+// const fermerMenu = () => {
+//   // Récupérer le menu
+//   const input = document.getElementById('menu-cb')
+//   input.checked = false
+//
+//   const fenetreNode = document.getElementById('menu-cote')
+//   fenetreNode.remove()
+// }
+//
+// const changerEtatMenu = () => {
+//   // Récupérer le menu
+//   const input = document.getElementById('menu-cb')
+//   const actif = input.checked
+//
+//   if (actif) {
+//     const fenetreNode = document.createElement('div')
+//     fenetreNode.id = 'menu-cote'
+//     fenetreNode.className = 'menu-cote'
+//     fenetreNode.addEventListener('click', fermerMenu)
+//     document.body.appendChild(fenetreNode)
+//   } else {
+//     const fenetreNode = document.getElementById('menu-cote')
+//     fenetreNode.remove()
+//   }
+// }
+//
+// const input = document.getElementById('menu-cb')
+// input.addEventListener('click', changerEtatMenu)
